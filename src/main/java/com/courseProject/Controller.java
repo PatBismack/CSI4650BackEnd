f0df2c82b236d51cabe2a4ba374ac45d964f5eda
patrickbismack <patrickbisma19@gmail.com>
2023-11-03 11:19:58 -0400

    Added new endpoint to get the vote totals

--- src/main/java/com/courseProject/Controller.java
+++ src/main/java/com/courseProject/Controller.java
@@ -10,33 +10,66 @@ import java.util.*;
 @RestController
 public class Controller {
 
-    @GetMapping("/get") List<Votes> Read(){
+    @GetMapping("/getAllVotes") List<Votes> Read(){
+        int vote;
         String voteValue;
         String id;
-        int vote;
 
         String CSVpath = "src/main/java/com/courseProject/Votes.csv";
 
         List <Votes> voteList = new ArrayList<>();
         Csv file = new Csv(CSVpath);
             String[][] data = file.readCSV();
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 0; i < data.length; i++){
             voteValue = data[i][0];
                 vote = Integer.parseInt(voteValue);
             id = data[i][1];
                 voteList.add(new Votes(vote, id));
         }
-        return voteList;
+            return voteList;
     }
 
-    @PostMapping("/post") String Write(@RequestBody Votes votes){
-        String id = votes.getId();
+    @GetMapping("/getTotal") Total voteTotal(@RequestBody Total total){
+        int vote;
+        int yes = 0;
+        int no = 0;
+        int dontCare = 0;
+        String voteValue;
+        String id;
+
+        String CSVpath = "src/main/java/com/courseProject/Votes.csv";
+
+        Total voteTotal = new Total();
+
+        Csv file = new Csv(CSVpath);
+        String[][] data = file.readCSV();
+        for (int i = 0; i < data.length; i++){
+            voteValue = data[i][0];
+            vote = Integer.parseInt(voteValue);
+            if(vote == 0){
+                yes++;
+                voteTotal.setYes(yes);
+            }
+            else if(vote == 1){
+                no++;
+                voteTotal.setNo(no);
+            }
+            else if(vote == 2){
+                dontCare++;
+                voteTotal.setDontCare(dontCare);
+            }
+        }
+            return voteTotal;
+    }
+
+    @PostMapping("/vote") String Write(@RequestBody Votes votes){
         int vote = votes.getVote();
+        String id = votes.getId();
 
         String CSVpath = "src/main/java/com/courseProject/Votes.csv";
 
         Csv file = new Csv(CSVpath);
             file.appendVote(vote, id);
-        return "Vote was recorded successfully: Vote " + vote + " Id: " + id;
+                return "Vote was recorded successfully: Vote " + vote + " Id: " + id;
     }
 }
